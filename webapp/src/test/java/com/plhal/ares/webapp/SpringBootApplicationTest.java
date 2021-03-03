8204c16b33cd76004b7dd4b14ee9d0959b879006
jiri-plhal <jirka.plhal89@gmail.com>
2021-03-03 09:00:57 +0100

    Pridan test spousteni aplikace

---
+++ webapp/src/test/java/com/plhal/ares/webapp/SpringBootApplicationTest.java
@@ -0,0 +1,14 @@
+package com.plhal.ares.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootApplicationTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
